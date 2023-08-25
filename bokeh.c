979388adebd640f259fe70679899bb88ae7f246b
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-08-25 11:56:47 -0400

    Add files via upload

--- bokeh.c
+++ bokeh.c
@@ -31,7 +31,7 @@ id=2 multiply aux=[ ref=2 color-overlay value=#ff1300 ]
 gaussian-blur std-dev-x=0 std-dev-y=0
 median-blur radius=0
 
-This Plugin shows two different ways to embed a color. GEGL Graph and GEGL color embedding.
+This Plugin shows two different ways to embed a color. GEGL Graph strings "hiddencolorbokeh" and GEGL color embedding "hiddencolorbokeh2".
  */
 
 #include "config.h"
@@ -40,12 +40,13 @@ This Plugin shows two different ways to embed a color. GEGL Graph and GEGL color
 #ifdef GEGL_PROPERTIES
 
 
-#define embeddedcolor \
+#define hiddencolorbokeh \
 " gegl:color value=#ff23cf "\
 
 
+
 /*Silly words like neighborhoodo are here so it does not conflict with Median Blur's ENUM list - that is why I added the o's
-NO TWO FILTERS CAN SHARE THE SAME ENUM LIST*/
+NO TWO FILTERS CAN SHARE THE SAME ENUM LIST NAME*/
 enum_start (gegl_median_blur_neighborhoodo)
   enum_value (GEGL_MEDIAN_BLUR_NEIGHBORHOOD_SQUAREo,  "square",  N_("Square"))
   enum_value (GEGL_MEDIAN_BLUR_NEIGHBORHOOD_CIRCLEo,  "circle",  N_("Circle"))
@@ -56,34 +57,31 @@ enum_end (GeglMedianBlurNeighborhoodo)
 property_enum (neighborhood, _("Shape"),
                GeglMedianBlurNeighborhoodo, gegl_median_blur_neighborhoodo,
                GEGL_MEDIAN_BLUR_NEIGHBORHOOD_CIRCLEo)
-  description (_("Neighborhood type"))
+  description (_("Median Shapes for the bokeh. Circle, Square and Diamond are the options."))
 
-property_color  (mcol, _("Color of the Bokeh"), "#ffffff")
+property_color  (color, _("Color of the Bokeh"), "#ffffff")
 
-property_double  (scale, _("Amount of Bokeh shapes"), 0.12)
-    description  (_("The scale of the noise function that increases the amount of shapes"))
+property_double  (amount, _("Amount of Bokeh shapes"), 0.12)
+    description  (_("The scale of the noise function that increases the amount of individual bokeh shapes"))
     value_range  (0.050, 0.35)
 
-property_double  (shape, _("Clarity of Background Bokehs"), 3.0)
-    description  (_("Interpolate between Manhattan and Euclidean distance."))
-    value_range  (0.0, 4.0)
 
 property_seed    (seed, _("Random seed"), rand)
     description  (_("The random seed for the noise function"))
 
-property_int  (median, _("Internal Median to increase Bokeh Size"), 25)
+property_int  (size, _("Internal Median to increase Bokeh Size"), 25)
   value_range (1, 80)
   ui_range    (1, 80)
   ui_meta     ("unit", "pixel-distance")
-  description (_("Median Blur Radius to increase the size of the bokeh. Neighborhood radius, a negative value will calculate with inverted percentiles"))
+  description (_("Median Blur Radius to increase the size of the bokeh. "))
 
-property_double (opacity, _("Opacity Increase/Decrease"), 0.6)
-    description (_("Global opacity value that is always used on top of the optional auxiliary input buffer."))
+property_double (opacity, _("Opacity Control"), 0.6)
+    description (_("Global opacity value for the bokehs."))
     value_range (0.1, 1.3)
     ui_range    (0.1, 1.3)
 
-property_int (lens, _("Blur"), 2)
-   description(_("Blurring tiny Bokehs will make it snow"))
+property_int (blur, _("Blur"), 2)
+   description(_("Blurring tiny bokehs will make it snow"))
    value_range (0, 12)
    ui_range    (0, 12)
    ui_gamma   (1.5)
@@ -101,25 +99,20 @@ property_int (lens, _("Blur"), 2)
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglNode *input, *output, *c2a, *color, *opacity, *median, *divide, *idref, *repairgeglgraph,  *mcol, *coloroverlay, *blur, *noise;
-
-  GeglColor *embeddedcolorbokehtwo = gegl_color_new ("#ff23cf");
-
+  GeglNode *input, *output, *c2a, *color, *opacity, *replace, *putinside, *median, *divide, *crop, *repairgeglgraph, *coloroverlay, *blur, *noise;
+  GeglColor *hiddencolorbokeh2 = gegl_color_new ("#ff23cf");
 
 
   input    = gegl_node_get_input_proxy (gegl, "input");
   output   = gegl_node_get_output_proxy (gegl, "output");
 
-c2a = gegl_node_new_child (gegl,
-                                "operation", "gegl:color-to-alpha", "transparency-threshold", 0.5, "color", embeddedcolorbokehtwo, NULL);
-                               
-  color    = gegl_node_new_child (gegl,
-                                  "operation", "gegl:gegl", "string", embeddedcolor,
-                                  NULL);
 
-idref = gegl_node_new_child (gegl,
-                                  "operation", "gegl:nop",
-                                  NULL);
+ 
+  c2a = gegl_node_new_child (gegl,
+                                  "operation", "gegl:color-to-alpha", "transparency-threshold", 0.5, "color", hiddencolorbokeh2, NULL);
+                               
+color = gegl_node_new_child (gegl,
+                                  "operation", "gegl:gegl", "string", hiddencolorbokeh, NULL);
                                
 opacity = gegl_node_new_child (gegl,
                                   "operation", "gegl:opacity",
@@ -129,9 +122,6 @@ median = gegl_node_new_child (gegl,
                                   NULL);
 divide = gegl_node_new_child (gegl,
                                   "operation", "gegl:divide",
-                                  NULL);
-mcol = gegl_node_new_child (gegl,
-                                  "operation", "gegl:multiply",
                                   NULL);
 
 coloroverlay = gegl_node_new_child (gegl,
@@ -145,6 +135,18 @@ blur = gegl_node_new_child (gegl,
                                   "operation", "gegl:lens-blur",
                                   NULL);
 
+replace = gegl_node_new_child (gegl,
+                                  "operation", "gegl:src",
+                                  NULL);
+
+crop = gegl_node_new_child (gegl,
+                                  "operation", "gegl:crop",
+                                  NULL);
+
+putinside = gegl_node_new_child (gegl,
+                                  "operation", "gegl:nop",
+                                  NULL);
+
 
   repairgeglgraph      = gegl_node_new_child (gegl, "operation", "gegl:median-blur",
                                          "radius",       0,
@@ -156,19 +158,21 @@ This option resets gegl:opacity's value to prevent a known bug where
 plugins like clay, glossy balloon and custom bevel glitch out when
 drop shadow is applied in a gegl graph below them.*/
  
-  gegl_node_link_many (input, color, divide, median, c2a, idref, mcol, opacity, blur, repairgeglgraph, output, NULL);
-  gegl_node_link_many (idref, coloroverlay, NULL);
-  gegl_node_connect_from (mcol, "aux", coloroverlay, "output");
+  gegl_node_link_many (input, putinside, replace, crop, output, NULL);
+  gegl_node_connect_from (replace, "aux", repairgeglgraph, "output");
+  gegl_node_link_many (putinside, color, divide, median, c2a, coloroverlay, opacity, blur, repairgeglgraph, NULL);
+
   gegl_node_connect_from (divide, "aux", noise, "output");
 
+    gegl_operation_meta_redirect (operation, "value", color, "value");
     gegl_operation_meta_redirect (operation, "opacity", opacity, "value");
-    gegl_operation_meta_redirect (operation, "median", median, "radius");
-    gegl_operation_meta_redirect (operation, "mcol", coloroverlay, "value");
+    gegl_operation_meta_redirect (operation, "size", median, "radius");
+    gegl_operation_meta_redirect (operation, "color", coloroverlay, "value");
     gegl_operation_meta_redirect (operation, "neighborhood", median, "neighborhood");
-    gegl_operation_meta_redirect (operation, "scale", noise, "scale");
+    gegl_operation_meta_redirect (operation, "amount", noise, "scale");
     gegl_operation_meta_redirect (operation, "shape", noise, "shape");
     gegl_operation_meta_redirect (operation, "seed", noise, "seed");
-    gegl_operation_meta_redirect (operation, "lens", blur, "radius");
+    gegl_operation_meta_redirect (operation, "blur", blur, "radius");
 
 }
 
@@ -182,9 +186,8 @@ gegl_op_class_init (GeglOpClass *klass)
   operation_class->attach = attach;
 
   gegl_operation_class_set_keys (operation_class,
-    "name",        "gegl:bokeh",
-    "title",       _("Bokeh Effect -Requires Alpha Channel"),
-    "categories",  "Generic",
+    "name",        "lb:bokeh",
+    "title",       _("Bokeh Effect -Requires Alpha Channel "),
     "reference-hash", "1a1210akk00k101x2001b2hc",
     "description", _("Create a fake bokeh effect using GEGL. For edits directly on top of the image without layers use the normal or other blending options (like overlay or softlight). If the image is glitching it is because your layer has no alpha channel. "
                      ""),
